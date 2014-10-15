b841abb231535bfd1b1cb79b71b1239a32f0d309
Justin Herrick <justin@justinherrick.com>
2014-10-14 22:40:49 -0400

    Adding issues.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('The Iron Yard Rocks');
+var issueTemplate = _.template( $('#issueTemplate').html() );
+var issues = $('#issues');
+var APIURL = 'https://api.github.com/repos/jah2488/github-issues/issues';
+
+
+var getIssues = function () {
+  $.ajax({url: APIURL}).done(function (data) {
+    var templates = _.map(data, function (issue) {
+      return issueTemplate(issue);
+    });
+    issues.html(templates);
+  });
+};
+
+setInterval(getIssues, 1000);
